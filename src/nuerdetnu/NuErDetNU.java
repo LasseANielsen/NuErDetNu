7a21e9b7b269adbec829a9f986ab7f86fd8c5746
Michael <mich561d@hotmail.com>
2018-09-07 12:44:34 +0200

    Test

--- src/nuerdetnu/NuErDetNU.java
+++ src/nuerdetnu/NuErDetNU.java
@@ -15,7 +15,9 @@ public class NuErDetNU {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String one = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD87uArXRJXykQ/4m1/ROaziLEXiEnsrkmCi0PrHP9H7jCcUlYdm6qOh1zgy8I1wETk/zyIkJRuU9JMSVfBrmh+uNwox2n7/FECqMm0Lbbmmogk6h5k+isLX0H3W3Ye6RjrR/bOrzHq9HOQwMwpFvtI2q/xV1uSXPvIdOAZtm86jt8DkLkxLwWR/XOJnI2MKQT1M8JmPCtjb2OoViJE7ZjAVYOIZ7eD49ciJiuZLUrPVMI1bfyzP8/nFHfRh04Gy5JnPkVh96EPbbfjp+6dJ3XB5eDvrLTI/xQTIcrO1hI5KvmyG7uyOOfDIirTQloM2kYqzPfUNF1pfXdo4QMxzmG3 lasse@Lasses-MacBook-Pro.local";
+        String two = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD87uArXRJXykQ/4m1/ROaziLEXiEnsrkmCi0PrHP9H7jCcUlYdm6qOh1zgy8I1wETk/zyIkJRuU9JMSVfBrmh+uNwox2n7/FECqMm0Lbbmmogk6h5k+isLX0H3W3Ye6RjrR/bOrzHq9HOQwMwpFvtI2q/xV1uSXPvIdOAZtm86jt8DkLkxLwWR/XOJnI2MKQT1M8JmPCtjb2OoViJE7ZjAVYOIZ7eD49ciJiuZLUrPVMI1bfyzP8/nFHfRh04Gy5JnPkVh96EPbbfjp+6dJ3XB5eDvrLTI/xQTIcrO1hI5KvmyG7uyOOfDIirTQloM2kYqzPfUNF1pfXdo4QMxzmG3 lasse@Lasses-MacBook-Pro.local";
+        System.out.println(one.equals(two));
     }
     
 }
